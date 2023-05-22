dcf27267ef599e887d6f5f964d7688ecdfda3fd1
youmjb <57849476+youmjb@users.noreply.github.com>
2023-05-22 15:36:45 +0900

    Add files via upload

---
+++ Menu.h
@@ -0,0 +1,35 @@
+/* Copyright 2022 the SumatraPDF project authors (see AUTHORS file).
+   License: GPLv3 */
+
+struct BuildMenuCtx;
+struct MenuOwnerDrawInfo;
+
+struct MenuDef {
+    const char* title = nullptr;
+    UINT_PTR idOrSubmenu = 0;
+};
+
+constexpr const char* kMenuSeparator = "-----";
+
+extern bool gShowDebugMenu;
+extern bool gAddCrashMeMenu;
+
+void FreeAllMenuDrawInfos();
+void FreeMenuOwnerDrawInfo(MenuOwnerDrawInfo*);
+void MarkMenuOwnerDraw(HMENU);
+void FreeMenuOwnerDrawInfoData(HMENU);
+void MenuOwnerDrawnMesureItem(HWND, MEASUREITEMSTRUCT*);
+void MenuOwnerDrawnDrawItem(HWND, DRAWITEMSTRUCT*);
+HFONT GetMenuFont();
+
+HMENU BuildMenuFromMenuDef(MenuDef* menuDefs, HMENU menu, BuildMenuCtx* ctx);
+void RemoveBadMenuSeparators(HMENU menu);
+HMENU BuildMenu(MainWindow* win);
+void OnWindowContextMenu(MainWindow* win, int x, int y);
+void OnCreateFreeText(MainWindow* win, int x, int y);
+void OnAboutContextMenu(MainWindow* win, int x, int y);
+int MenuIdFromVirtualZoom(float virtualZoom);
+void UpdateAppMenu(MainWindow* win, HMENU m);
+void ToggleMenuBar(MainWindow* win, bool showTemporarily = false);
+float ZoomMenuItemToZoom(int menuItemId);
+void ShowFileInFolder(const char* path);
